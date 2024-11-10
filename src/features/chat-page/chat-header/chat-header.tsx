32a784c4ccf45d542f16dd204470848bdd2ff10c
ivan20203 <ivanpupo2023@gmail.com>
2024-11-09 20:58:46 -0800

    make look nice

--- src/features/chat-page/chat-header/chat-header.tsx
+++ src/features/chat-page/chat-header/chat-header.tsx
@@ -31,13 +31,13 @@ export const ChatHeader: FC<Props> = (props) => {
         </div>
         <div className="flex gap-2">
           <PersonaDetail chatThread={props.chatThread} />
-          <DocumentDetail chatDocuments={props.chatDocuments} />
+          {/* <DocumentDetail chatDocuments={props.chatDocuments} />
           <ExtensionDetail
             disabled={props.chatDocuments.length !== 0}
             extensions={props.extensions}
             installedExtensionIds={props.chatThread.extension}
             chatThreadId={props.chatThread.id}
-          />
+          /> */}
         </div>
       </div>
     </div>
--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "RolePlayShow default";
+export const AI_DESCRIPTION = "RolePlayShow is an AI rpg storyteller.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI rpg storyteller. You must always return in markdown format.
 
 
 `;
